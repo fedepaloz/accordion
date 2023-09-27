fd752cbce1897586ff155b7d69257f5d0d9979d8
fedepaloz <palozzifederico@gmail.com>
2023-09-27 17:57:10 +0200

    static

--- src/App.js
+++ src/App.js
@@ -1,7 +1,45 @@
-import "./App.css";
+import "./styles.css";
 
-function App() {
-  return <div className="App">ko</div>;
+const faqs = [
+  {
+    title: "Where are these chairs assembled?",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
+  },
+  {
+    title: "How long do I have to return my chair?",
+    text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
+  },
+  {
+    title: "Do you ship to countries outside the EU?",
+    text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
+  },
+];
+
+export default function App() {
+  return (
+    <div>
+      <Accordion data={faqs} />
+    </div>
+  );
 }
 
-export default App;
+function Accordion({ data }) {
+  return (
+    <div className="accordion">
+      {data.map((el, i) => (
+        <AccordionItem title={el.title} text={el.text} num={i} />
+      ))}
+    </div>
+  );
+}
+
+function AccordionItem({ num, title, text }) {
+  return (
+    <div className="item">
+      <p className="number">{num}</p>
+      <p className="text">{text}</p>
+      <p className="icon">-</p>
+      <div className="content-box">{text}</div>
+    </div>
+  );
+}
